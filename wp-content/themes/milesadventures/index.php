b9ec495ba2e277c5cdfa15224be52625456612e3
Trevor Miles <miles.trev5@gmail.com>
2022-08-16 21:31:47 -0700

    Stylize the index

--- wp-content/themes/milesadventures/index.php
+++ wp-content/themes/milesadventures/index.php
@@ -1,1 +1,4 @@
-<h1>Miles Adventures</h1>
+<div style="width: 100%; height: 100vh; display: flex; align-items: center; flex-direction: column; justify-content: center;">
+    <h1 style="font-family: helvetica; color: #262626; margin: 0; font-size: 48px;">Miles Adventures</h1>
+    <p style="font-family: helvetica; color: #262626;">Coming soon</p>
+</div>
