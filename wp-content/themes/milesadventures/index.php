2cba0b50f02ce788b2c73e08ee287c8b7ef06bb6
Trevor Miles <miles.trev5@gmail.com>
2022-09-02 22:28:58 -0700

    Add foundation styles

--- wp-content/themes/milesadventures/index.php
+++ wp-content/themes/milesadventures/index.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
     <h1>miles adventures</h1>
+    <h2>miles adventures</h2>
+    <h3>miles adventures</h3>
+    <a href="/" class="btn">Read more</a>
 <?php get_footer(); ?>
