f7ee98b15c1fc67e8db425bc49479c4bb5d79467
Trevor Miles <miles.trev5@gmail.com>
2022-09-10 09:11:11 -0700

    Install Carbon Fields via Composer

--- wp-content/themes/milesadventures/functions.php
+++ wp-content/themes/milesadventures/functions.php
@@ -1,5 +1,22 @@
 <?php
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
+function crb_attach_theme_options() {
+    Container::make( 'theme_options', __( 'Theme Options' ) )
+        ->add_fields( array(
+            Field::make( 'text', 'crb_text', 'Text Field' ),
+        ) );
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+    require_once( 'vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+
 $manifest = json_decode(file_get_contents(get_stylesheet_directory() . "/mix-manifest.json"), true);
 
 add_action( 'wp_enqueue_scripts', function() use ( $manifest ) { enqueue_webpack_file( 'global.js', '/public/js/global.js', $manifest, 'js' ); } );
