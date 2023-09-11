f8796d358efa406ecbbff4c7471808024cb5bf04
Trevor Miles <miles.trev5@gmail.com>
2023-09-11 09:01:50 -0600

    Development of latest component

--- wp-content/themes/milesadventures/page_home.php
+++ wp-content/themes/milesadventures/page_home.php
@@ -2,6 +2,58 @@
 /*
   Template Name: Home
  */
+
+
+$adventures = [
+    [
+        'title' => 'Portland',
+        'description' => 'Silver Falls State Park, Columbia River Gorge & Mount Hood Wilderness Area',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-01-01',
+        'end_date' => '2022-02-01',
+    ],
+    [
+        'title' => 'Vancouver',
+        'description' => 'Golden Ears Provincial Park, Squamish, Pacific Rim National Park',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-02-01',
+        'end_date' => '2022-03-01',
+    ],
+    [
+        'title' => 'Redwood National Park',
+        'description' => 'Redwood NP, Jedidiah Smith Redwoods, Del Norte Coast Redwoods, Prairie Creek Redwoods',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-03-01',
+        'end_date' => '2022-04-01',
+    ],
+    [
+        'title' => 'Portland',
+        'description' => 'Silver Falls State Park, Columbia River Gorge & Mount Hood Wilderness Area',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-01-01',
+        'end_date' => '2022-02-01',
+    ],
+    [
+        'title' => 'Vancouver',
+        'description' => 'Golden Ears Provincial Park, Squamish, Pacific Rim National Park',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-02-01',
+        'end_date' => '2022-03-01',
+    ],
+    [
+        'title' => 'Redwood National Park',
+        'description' => 'Redwood NP, Jedidiah Smith Redwoods, Del Norte Coast Redwoods, Prairie Creek Redwoods',
+        'href' => '/',
+        'image_url' => '/wp-content/uploads/2022/09/nat-trev-hurricane.jpg',
+        'start_date' => '2022-03-01',
+        'end_date' => '2022-04-01',
+    ],
+];
 ?>
 
 <?php get_header(); ?>
@@ -17,6 +69,23 @@
                     Including the Hoh Rain Forest, Quinault Rain Forest, Sol Duc Falls, Lake Crescent & Hurricane Ridge.  
                 </p>
                 <a href="/" class="btn btn--solid-hover">Read More</a>
+            </div>
+        </div>
+    </section>
+    <section class="section-latest">
+        <div class="content-container content-container--2">
+            <h2>Latest adventures</h2>
+            <div class="section-latest__grid">
+                <?php foreach ($adventures as $item) : ?>
+                    <div class="item-preview-primary">
+                        <a href="<?= $item['href']; ?>" class="item-preview-primary__image-container">
+                            <img src="<?= $item['image_url'] ?>" class="item-preview-primary__image">
+                        </a>
+                        <h3><?= $item['title']; ?></h3>
+                        <p><?= $item['description']; ?></p>
+                        <a href="<?= $item['href']; ?>" class="btn">Read more</a>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
