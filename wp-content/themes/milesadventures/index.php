d9535bfdb6f801e73ae652c36579a2fe9b3b2088
Trevor Miles <miles.trev5@gmail.com>
2023-11-20 19:22:11 -0700

    Improvements to the index.php

--- wp-content/themes/milesadventures/index.php
+++ wp-content/themes/milesadventures/index.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
-    <div class="content-container">
-        <h1>Miles Out There</h1>
-    </div>
+<div class="content-container">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+    <?php else: ?>
+        <p>No posts found.</p>
+    <?php endif; ?>
+</div>
 <?php get_footer(); ?>
